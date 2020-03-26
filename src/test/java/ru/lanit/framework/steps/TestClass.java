26e24fae03aafb679ad7e3c61b9b536c2efcc920
gromoval <gromov-rabota@yandex.ru>
2020-03-26 19:53:27 +0300

    [+] Задание по Selenium

--- src/test/java/ru/lanit/framework/steps/TestClass.java
+++ src/test/java/ru/lanit/framework/steps/TestClass.java
@@ -7,28 +7,101 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import ru.lanit.framework.webdriver.WebDriverManager;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class TestClass {
+    private final Map<String, String> loginPasswordData = new HashMap<>();
+
+    @BeforeClass
+    private void checkAuth() {
+//        loginPasswordData.put("gromovalex", "1234qwerasdf"); будем проверять только невалидное. валид потом отдельно проверим
+        loginPasswordData.put("sdfkjhyfejrhsdfksdfh", "sdjfhksdjfhksdjfhyrgberjhbdf");
+        loginPasswordData.put("admin", "простокакойтопароль");
+        loginPasswordData.put("" , "");
+        loginPasswordData.put("", "abyvalg");
+        loginPasswordData.put("abyrvalg", "");
+    }
+
+    @AfterTest
+    void clearHashMap() {
+        loginPasswordData.clear();
+    }
 
     @Test
     public void testGoogle() throws InterruptedException {
         WebDriver webDriver = WebDriverManager.getDriver();
-        webDriver.get("http://google.com");
-        Thread.sleep(1000);
-        WebElement webElement = webDriver.findElement(By.name("q"));
-        List<WebElement> webElements = webDriver.findElements(By.xpath("//div"));
-        webElement.click();
-        Thread.sleep(1000);
-        webElement.sendKeys("Hello World!");
-        Thread.sleep(1000);
-        webElement.sendKeys(Keys.ENTER);
-        Assert.assertTrue(webDriver.findElement(By.xpath("//*[@id='result-stats']")).isDisplayed(), "Элемент \"Результатов: примерно...\"");
-        Thread.sleep(1000);
         System.out.println("Открыт браузер " + ((ChromeDriver) webDriver).getSessionId());
+        webDriver.get("https://dev.n7lanit.ru/");
+        WebElement webElement = webDriver.findElement(By.linkText("Категории"));
+//        webElement.click();
+//        System.out.println("Нашли и клинкули ссылку 'Категории'");
+//        Thread.sleep(1000);
+//        webElement = webDriver.findElement(By.linkText("Пользователи"));
+//        webElement.click();
+//        System.out.println("Нашли и клинкули ссылку 'Пользователи'");
+//        Thread.sleep(1000);
+//        webElement = webDriver.findElement(By.xpath("//div[@class='navbar-search dropdown']"));
+//        webElement.click();
+//        Thread.sleep(1000);
+//        webElement = webDriver.findElement(By.xpath("//*[@aria-controls='dropdown-menu dropdown-search-results']"));
+//        webElement.click();
+//        webElement.sendKeys("gromovalex");
+//        Thread.sleep(1000);
+//        webElement = webDriver.findElement(By.xpath("//*[@class='dropdown-search-user']"));
+//        webElement.click();
+//        Assert.assertTrue(webDriver.findElement(By.xpath("//abbr[@title='Присоединился 26 марта 2020 г., 11:35']")).isDisplayed(), "Присоединился");
+//        System.out.println("Нашли пользователя 'gromovalex'");
+//        Thread.sleep(1000);
+
+//        доп задание
+
+        for (Map.Entry<String, String> entry : loginPasswordData.entrySet()) {
+            webElement = webDriver.findElement(By.xpath("//button[@class='btn navbar-btn btn-default btn-sign-in']"));
+            webElement.click();
+            Thread.sleep(1000);
+            String username =  entry.getKey();
+            String password = entry.getValue();
+            WebElement webElementLogin = webDriver.findElement(By.xpath("//input[@id='id_username']"));
+            webElementLogin.clear();
+            webElementLogin.click();
+            webElementLogin.sendKeys(username.toString());
+            WebElement webElementPassword = webDriver.findElement(By.xpath("//input[@id='id_password']"));
+            webElementPassword.clear();
+            webElementPassword.click();
+            webElementPassword.sendKeys(password.toString());
+            webElementLogin.sendKeys(Keys.ENTER);
+            webElement.findElement(By.xpath("//div[@class='alerts-snackbar in']"));
+            if (webElement.isDisplayed()) {
+                System.out.println("Неверные параметры. Авторизоваться нет возможности! Проверка пройдена!");
+                Thread.sleep(4000); // закрывает кнопку красная надпись, попасть по элементу не может если меньше сделать. так она сама пропадет
+                webElementLogin = webDriver.findElement(By.xpath("//button[@class='close' and @aria-label='Закрыть']"));
+                webElementLogin.click();
+            }
+        }
+
+        WebElement webElement1 = webDriver.findElement(By.xpath("//button[@class='btn navbar-btn btn-default btn-sign-in']"));
+        webElement1.click();
+        Thread.sleep(1000);
+        String password1 = "1234qwerasdf";
+        WebElement webElementLogin1 = webDriver.findElement(By.xpath("//input[@id='id_username']"));
+        webElementLogin1.clear();
+        webElementLogin1.click();
+        webElementLogin1.sendKeys("gromovalex");
+        WebElement webElementPassword1 = webDriver.findElement(By.xpath("//input[@id='id_password']"));
+        webElementPassword1.clear();
+        webElementPassword1.click();
+        webElementPassword1.sendKeys("1234qwerasdf");
+        webElementLogin1.sendKeys(Keys.ENTER);
+        Thread.sleep(1000);
+        Assert.assertTrue(webDriver.findElement(By.xpath("//a[@class='dropdown-toggle' and @data-toggle='dropdown']")).isDisplayed());
+        System.out.println("Пользователь авторизован! Тест пройден успешно");
         webDriver.quit();
 
     }
