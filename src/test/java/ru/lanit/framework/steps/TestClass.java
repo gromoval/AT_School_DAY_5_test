9dc1183f1400239786ed418ecddecb63e17f7b06
gromoval <gromov-rabota@yandex.ru>
2020-03-27 16:11:34 +0300

    [+] Нормально отрабатывает. сделал обработчик отсутствия объекта на странице. вожусь с алертом на выход

--- src/test/java/ru/lanit/framework/steps/TestClass.java
+++ src/test/java/ru/lanit/framework/steps/TestClass.java
@@ -1,11 +1,9 @@
 package ru.lanit.framework.steps;
 
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
@@ -13,15 +11,15 @@ import org.testng.annotations.Test;
 import ru.lanit.framework.webdriver.WebDriverManager;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
-public class TestClass {
+public class TestClass extends BasePageSteps {
     private final Map<String, String> loginPasswordData = new HashMap<>();
 
     @BeforeClass
     private void checkAuth() {
-        loginPasswordData.put("gromovalex", "1234qwerasdf"); // будем проверять только невалидное. валид потом отдельно проверим
+        loginPasswordData.put("gromovalex", "1234qwerasdf");
         loginPasswordData.put("sdfkjhyfejrhsdfksdfh", "sdjfhksdjfhksdjfhyrgberjhbdf");
         loginPasswordData.put("admin", "простокакойтопароль");
         loginPasswordData.put("" , "");
@@ -33,6 +31,18 @@ public class TestClass {
     void clearHashMap() {
         loginPasswordData.clear();
     }
+
+    private boolean isElementPresent(String locator_string){
+        try {
+            WebDriverManager.getDriver().manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+            return WebDriverManager.getDriver().findElement(By.xpath(locator_string)).isDisplayed();
+        } catch (NoSuchElementException e){
+            return false;
+        } finally {
+            WebDriverManager.getDriver().manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
+        }
+    }
+
 
     @Test
     public void testGoogle() throws InterruptedException {
@@ -56,7 +66,7 @@ public class TestClass {
 //        Thread.sleep(1000);
 //        webElement = webDriver.findElement(By.xpath("//*[@class='dropdown-search-user']"));
 //        webElement.click();
-//        Assert.assertTrue(webDriver.findElement(By.xpath("//abbr[@title='Присоединился 26 марта 2020 г., 11:35']")).isDisplayed(), "Присоединился");
+//        Assert.assertTrue(webDriver.findElement(By.xpath("//abbr[@title='Присоединился 26 марта 2020 г., 11:35']")).isDisplayed());
 //        System.out.println("Нашли пользователя 'gromovalex'");
 //        Thread.sleep(1000);
 
@@ -77,33 +87,30 @@ public class TestClass {
             webElementPassword.click();
             webElementPassword.sendKeys(password.toString());
             webElementLogin.sendKeys(Keys.ENTER);
-            webElement.findElement(By.xpath("//div[@class='alerts-snackbar in']"));
-            if (webElement.isDisplayed()) {
-                System.out.println("Неверные параметры. Авторизоваться нет возможности! Проверка пройдена!");
+            Thread.sleep(1000);
+
+            if (isElementPresent("//div[@class='alerts-snackbar in']")) {
+                System.out.println("Неверные параметры. Авторизоваться нет возможности! Проверка пройдена!"); // там 2 вида вывода, или поля пустые или неверный логин пароль. причем это все выведено через 1 элемент, не разделить их
                 Thread.sleep(4000); // закрывает кнопку красная надпись, попасть по элементу не может если меньше сделать. так она сама пропадет
                 webElementLogin = webDriver.findElement(By.xpath("//button[@class='close' and @aria-label='Закрыть']"));
                 webElementLogin.click();
             } else {
-                System.out.println("Пользователь авторизован! Проверка пройдена!");
+                System.out.println("Пользователь "+username+" авторизован! Проверка пройдена!");
+                wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//img[@class='user-avatar' and @width='64']")));
+                webElement = webDriver.findElement(By.xpath("//img[@class='user-avatar' and @width='64']"));
+                webElement.click();
+                Thread.sleep(100);
+                webElement = webDriver.findElement(By.xpath("//button[@class='btn btn-default btn-block']"));
+                try {
+                    webElement.click();
+                } catch (org.openqa.selenium.UnhandledAlertException e) {
+                    Alert alert = webDriver.switchTo().alert();
+                    System.out.println("sdfsdfsfsfsdfsdfsdfsf");
+                    alert.accept();
+                }
             }
         }
 
-//        WebElement webElement1 = webDriver.findElement(By.xpath("//button[@class='btn navbar-btn btn-default btn-sign-in']"));
-//        webElement1.click();
-//        Thread.sleep(1000);
-//        WebElement webElementLogin1 = webDriver.findElement(By.xpath("//input[@id='id_username']"));
-//        webElementLogin1.clear();
-//        webElementLogin1.click();
-//        webElementLogin1.sendKeys("gromovalex");
-//        WebElement webElementPassword1 = webDriver.findElement(By.xpath("//input[@id='id_password']"));
-//        webElementPassword1.clear();
-//        webElementPassword1.click();
-//        webElementPassword1.sendKeys("1234qwerasdf");
-//        webElementLogin1.sendKeys(Keys.ENTER);
-//        Thread.sleep(1000);
-//        Assert.assertTrue(webDriver.findElement(By.xpath("//a[@class='dropdown-toggle' and @data-toggle='dropdown']")).isDisplayed());
-//        System.out.println("Пользователь авторизован! Тест пройден успешно");
-//        webDriver.quit();
-
+        webDriver.quit();
     }
 }
