c198b01446e9c7eefe12a8b81d2973449c3d9dd2
gromoval <gromov-rabota@yandex.ru>
2020-03-27 17:00:47 +0300

    [+] It's ALIVE!!!! (все отрабатывает как задумано!)

--- src/test/java/ru/lanit/framework/steps/TestClass.java
+++ src/test/java/ru/lanit/framework/steps/TestClass.java
@@ -45,30 +45,30 @@ public class TestClass extends BasePageSteps {
 
 
     @Test
-    public void testGoogle() throws InterruptedException {
+    public void testGoogle() throws InterruptedException, UnhandledAlertException {
         WebDriver webDriver = WebDriverManager.getDriver();
         System.out.println("Открыт браузер " + ((ChromeDriver) webDriver).getSessionId());
         webDriver.get("https://dev.n7lanit.ru/");
         WebElement webElement = webDriver.findElement(By.linkText("Категории"));
-//        webElement.click();
-//        System.out.println("Нашли и клинкули ссылку 'Категории'");
-//        Thread.sleep(1000);
-//        webElement = webDriver.findElement(By.linkText("Пользователи"));
-//        webElement.click();
-//        System.out.println("Нашли и клинкули ссылку 'Пользователи'");
-//        Thread.sleep(1000);
-//        webElement = webDriver.findElement(By.xpath("//div[@class='navbar-search dropdown']"));
-//        webElement.click();
-//        Thread.sleep(1000);
-//        webElement = webDriver.findElement(By.xpath("//*[@aria-controls='dropdown-menu dropdown-search-results']"));
-//        webElement.click();
-//        webElement.sendKeys("gromovalex");
-//        Thread.sleep(1000);
-//        webElement = webDriver.findElement(By.xpath("//*[@class='dropdown-search-user']"));
-//        webElement.click();
-//        Assert.assertTrue(webDriver.findElement(By.xpath("//abbr[@title='Присоединился 26 марта 2020 г., 11:35']")).isDisplayed());
-//        System.out.println("Нашли пользователя 'gromovalex'");
-//        Thread.sleep(1000);
+        webElement.click();
+        System.out.println("Нашли и клинкули ссылку 'Категории'");
+        Thread.sleep(1000);
+        webElement = webDriver.findElement(By.linkText("Пользователи"));
+        webElement.click();
+        System.out.println("Нашли и клинкули ссылку 'Пользователи'");
+        Thread.sleep(1000);
+        webElement = webDriver.findElement(By.xpath("//div[@class='navbar-search dropdown']"));
+        webElement.click();
+        Thread.sleep(1000);
+        webElement = webDriver.findElement(By.xpath("//*[@aria-controls='dropdown-menu dropdown-search-results']"));
+        webElement.click();
+        webElement.sendKeys("gromovalex");
+        Thread.sleep(1000);
+        webElement = webDriver.findElement(By.xpath("//*[@class='dropdown-search-user']"));
+        webElement.click();
+        Assert.assertTrue(webDriver.findElement(By.xpath("//abbr[@title='Присоединился 26 марта 2020 г., 11:35']")).isDisplayed());
+        System.out.println("Нашли пользователя 'gromovalex'");
+        Thread.sleep(1000);
 
 //        доп задание
 
@@ -91,22 +91,23 @@ public class TestClass extends BasePageSteps {
 
             if (isElementPresent("//div[@class='alerts-snackbar in']")) {
                 System.out.println("Неверные параметры. Авторизоваться нет возможности! Проверка пройдена!"); // там 2 вида вывода, или поля пустые или неверный логин пароль. причем это все выведено через 1 элемент, не разделить их
+                Assert.assertTrue(webDriver.findElement(By.xpath("//div[@class='alerts-snackbar in']")).isDisplayed());
                 Thread.sleep(4000); // закрывает кнопку красная надпись, попасть по элементу не может если меньше сделать. так она сама пропадет
                 webElementLogin = webDriver.findElement(By.xpath("//button[@class='close' and @aria-label='Закрыть']"));
                 webElementLogin.click();
             } else {
                 System.out.println("Пользователь "+username+" авторизован! Проверка пройдена!");
                 wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//img[@class='user-avatar' and @width='64']")));
+                Assert.assertTrue(webDriver.findElement(By.xpath("//img[@class='user-avatar' and @width='64']")).isDisplayed());
                 webElement = webDriver.findElement(By.xpath("//img[@class='user-avatar' and @width='64']"));
                 webElement.click();
                 Thread.sleep(100);
                 webElement = webDriver.findElement(By.xpath("//button[@class='btn btn-default btn-block']"));
                 try {
                     webElement.click();
+                    Thread.sleep(1000);
+                    webDriver.switchTo().alert().accept();
                 } catch (org.openqa.selenium.UnhandledAlertException e) {
-                    Alert alert = webDriver.switchTo().alert();
-                    System.out.println("sdfsdfsfsfsdfsdfsdfsf");
-                    alert.accept();
                 }
             }
         }
